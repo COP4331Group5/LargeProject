7d4f807e9eb1b06e1ed158582d092d168d7659a0
Eric L <eric.leiva@knights.ucf.edu>
2019-03-27 14:09:57 -0400

    created app.js and added reference to it in index.html

---
+++ public/app.js
@@ -0,0 +1,14 @@
+(function() {
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBvLvgjqeKw8R0juoj3adwtqSSJ7BoHVn4",
+  authDomain: "large-project-spring-2019.firebaseapp.com",
+  databaseURL: "https://large-project-spring-2019.firebaseio.com",
+  projectId: "large-project-spring-2019",
+  storageBucket: "large-project-spring-2019.appspot.com",
+  messagingSenderId: "495602203108"
+};
+firebase.initializeApp(config);
+
+}());
